9479c10df05539f0ca1c06c4fe86b033c3ac3d0b
MartMbithi <martdevelopers254@gmail.com>
2020-08-27 12:19:49 +0300

    Attache Base Assets

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Mart Developers
                 </div>
 
                 <div class="links">
