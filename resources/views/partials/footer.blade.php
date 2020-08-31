0d00819aef6cb9a1d33f1fb031d4eab285929306
MartMbithi <martdevelopers254@gmail.com>
2020-08-31 17:45:24 +0300

    Multiple Bug Fixes

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -3,11 +3,10 @@
         <div class="row">
             <div class="col-md-12 text-center">
                 <ul class="social-icons">
-                    <li class="wow bounceIn animated" data-wow-delay="0.1s"><a href="#."><i class="fa fa-facebook"></i></a></li>
-                    <li class="wow bounceIn animated" data-wow-delay="0.2s"><a href="#."><i class="fa fa-twitter"></i></a></li>
-                    <li class="wow bounceIn animated" data-wow-delay="0.3s"><a href="#."><i class="fa fa-google-plus"></i></a></li>
-                    <li class="wow bounceIn animated" data-wow-delay="0.4s"><a href="#."><i class="fa fa-dribbble"></i></a></li>
-                    <li class="wow bounceIn animated" data-wow-delay="0.5s"><a href="#."><i class="fa fa-pinterest"></i></a></li>
+                    <li class="wow bounceIn animated" data-wow-delay="0.1s"><a target="_blank" href="https://www.facebook.com/martin.mbithi.73"><i class="fa fa-facebook"></i></a></li>
+                    <li class="wow bounceIn animated" data-wow-delay="0.2s"><a target="_blank" href="https://twitter.com/martinezmbithi"><i class="fa fa-twitter"></i></a></li>
+                    <li class="wow bounceIn animated" data-wow-delay="0.3s"><a  href="mailto:martdevelopers254@gmail.com"><i class="fa fa-envelope"></i></a></li>
+                    <li class="wow bounceIn animated" data-wow-delay="0.4s"><a target="_blank" href="https://github.com/MartDevelopers-Inc"><i class="fa fa-github"></i></a></li>
                 </ul>
                 <a href="{{ url('/') }}"><img src="assets/img/MartDevelopers-Website-logo.png" class="footerlogo wow zoomIn" alt="Logo"></a>
                 <p class="copyright">
