f34cb8d0b062789cb552b0bb4d7edc5b64788d00
MartMbithi <martdevelopers254@gmail.com>
2020-08-31 17:37:47 +0300

    Refactored Home Page

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,19 @@
+<section id="footer" class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <ul class="social-icons">
+                    <li class="wow bounceIn animated" data-wow-delay="0.1s"><a href="#."><i class="fa fa-facebook"></i></a></li>
+                    <li class="wow bounceIn animated" data-wow-delay="0.2s"><a href="#."><i class="fa fa-twitter"></i></a></li>
+                    <li class="wow bounceIn animated" data-wow-delay="0.3s"><a href="#."><i class="fa fa-google-plus"></i></a></li>
+                    <li class="wow bounceIn animated" data-wow-delay="0.4s"><a href="#."><i class="fa fa-dribbble"></i></a></li>
+                    <li class="wow bounceIn animated" data-wow-delay="0.5s"><a href="#."><i class="fa fa-pinterest"></i></a></li>
+                </ul>
+                <a href="{{ url('/') }}"><img src="assets/img/MartDevelopers-Website-logo.png" class="footerlogo wow zoomIn" alt="Logo"></a>
+                <p class="copyright">
+                    &copy; MartDevelopers Inc. All rights reserved. Design by <a target="_blnak"  href="https://gfx.martde.info/">MartGFX</a>
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
