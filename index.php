f747760bb16d065069aacb57c9c85f7263987161
MartMbithi <martdevelopers254@gmail.com>
2020-09-01 16:28:43 +0300

    Routed Routes

--- index.php
+++ index.php
@@ -23,6 +23,7 @@ define('LARAVEL_START', microtime(true));
 
 require __DIR__.'/vendor/autoload.php';
 
+
 /*
 |--------------------------------------------------------------------------
 | Turn On The Lights
@@ -35,7 +36,7 @@ require __DIR__.'/vendor/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/bootstrap/app.php';
+$app = require __DIR__.'/bootstrap/app.php';
 
 /*
 |--------------------------------------------------------------------------
